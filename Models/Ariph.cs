6713be7df5fa211c887d9b5b30fd22b12c1a7933
ZhannaKarpova <92864174+ZhannaKarpova@users.noreply.github.com>
2022-01-29 22:59:09 +0700

    Добавьте файлы проекта.

---
+++ Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLAB19.Models
+{
+    static class Ariph
+    {
+        public static double Add(int a) => 2 * a * Math.PI;
+    }
+}
